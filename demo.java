137f3192ab5be01dced003e389573e808751f14b
TharunTK007 <tharunkumarg13@gmail.com>
2025-09-21 21:33:45 +0530

    version2

--- demo.java
+++ demo.java
@@ -4,5 +4,6 @@ public class demo {
         System.out.println("Add : "+(10+20)); //30
         System.out.println("Sub : "+(20-10)); //10
         System.out.println("Mul : "+(10*10)); //100
+        System.out.println("div : "+(50/5)); //10
     }
 }

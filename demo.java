708d361067acd497f7befcee1f2ad6e303a1be7c
TharunTK007 <tharunkumarg13@gmail.com>
2025-09-21 20:50:17 +0530

    version2

--- demo.java
+++ demo.java
@@ -1,6 +1,8 @@
 public class demo {
 
     public static void main(String[] args) {
-        System.out.println("Add : "+(10+20));
+        System.out.println("Add : "+(10+20)); //30
+        System.out.println("Sub : "+(20-10)); //10
+        System.out.println("Mul : "+(10*10)); //100
     }
 }
